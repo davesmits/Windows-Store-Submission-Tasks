3acd15798633b6351eeec40726f3d4ade630919d
Dave Smits <dave@familie-smits.com>
2016-08-21 23:04:46 +0100

    Added task to update version

---
+++ WindowsStoreExtension/VersionUpdate/UpdateVersion.ps1
@@ -0,0 +1,27 @@
+Param(
+    [String]$manifestfile,
+    [String]$buildid
+)
+
+  
+if (-Not $manifestfile) {
+    Write-Host "No manifest file"
+    exit 1
+}
+
+if (-Not $buildid) {
+    Write-Host "no build Id set"
+    exit 1
+}
+
+  
+$file = Get-Item -Path "$manifestfile"
+  
+$manifestXml = New-Object -TypeName System.Xml.XmlDocument
+$manifestXml.Load($file.Fullname)
+
+$currentVersion = [Version]$manifestXml.Package.Identity.Version
+$updatedVersion = [Version]($currentVersion.Major.ToString() + '.' + $currentVersion.Minor + '.' + $buildid + '.' + 0)
+
+$manifestXml.Package.Identity.Version = [String]$updatedVersion
+$manifestXml.save($manifestfile.FullName)
