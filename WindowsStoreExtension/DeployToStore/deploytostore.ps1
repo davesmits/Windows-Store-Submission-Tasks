0906c8067f4952be9e61f0ede0543cbea55b1671
Dave Smits <dave@familie-smits.com>
2016-08-22 10:37:15 +0000

    Updated deploytostore.ps1

--- WindowsStoreExtension/DeployToStore/deploytostore.ps1
+++ WindowsStoreExtension/DeployToStore/deploytostore.ps1
@@ -7,6 +7,7 @@ param
    [Parameter(Mandatory=$true)][string] $fileurl
 )
 
+$ErrorActionPreference = "Stop"
 
 function MakeSubmission($tenantId, $clientId, $clientSecret)
 {
@@ -15,7 +16,7 @@ function MakeSubmission($tenantId, $clientId, $clientSecret)
  
     $accessToken = $token.access_token
 
-    Write-Host "accesstoken: $accessToken"
+    #Write-Host "accesstoken: $accessToken"
 
     $createSubmissionUrl = "https://manage.devcenter.microsoft.com/v1.0/my/applications/$appid/submissions"
     $submission = Invoke-WebRequest -Uri $createSubmissionUrl -Headers @{"Authorization" = "bearer $accessToken"} -Method POST -UseBasicParsing | ConvertFrom-Json
@@ -37,7 +38,7 @@ function MakeFlightSubmission($teantId, $clientId, $clientSecret, $flightId)
 
     $accessToken = $token.access_token
 
-    Write-Host "accesstoken: $accessToken"
+    #Write-Host "accesstoken: $accessToken"
 
     $createSubmissionUrl = "https://manage.devcenter.microsoft.com/v1.0/my/applications/$appid/flights/$flightId/submissions"
     $submission = Invoke-WebRequest -Uri $createSubmissionUrl -Headers @{"Authorization" = "bearer $accessToken"} -Method POST -UseBasicParsing | ConvertFrom-Json
@@ -52,31 +53,14 @@ function MakeFlightSubmission($teantId, $clientId, $clientSecret, $flightId)
     $submission = Invoke-WebRequest -Uri $commitSubmissionUrl -Headers @{"Authorization" = "bearer $accessToken"} -Body $submissionJson -Method POST -UseBasicParsing
 }
 
-#$fileUrl = "";
-#$appid = "9wzdncrfjmb6";
-
-#$tenantId = "baa64767-ef44-4db6-a573-9770147f3397"
-#$clientId = "2efafcca-cd37-47e1-8a7e-20908e7f1dc4"
-#$clientSecret = "IJodayZ/Z7E5vLQeJZH9L9lD7/jUJ9TAQNI9HrAFUno="
-#$flightId = "";
-
-#$tenantId = "baa64767-ef44-4db6-a573-9770147f3397"
-#$clientId = "2efafcca-cd37-47e1-8a7e-20908e7f1dc4"
-#$clientSecret = "+Cdjz+i7Nu4BUhFOip/gW8mjdPzMLWr3PlzOoykMmwM="
-
-#tenant dave
-#$tenantId = "61e615f3-161b-4bda-a67a-407317766d1f"
-#$clientId = "1d913b52-16ad-411a-a5e6-f09a60d5e1ec"
-#$clientSecret = "oTarctYJVHsiJCE9hk+M+gCPVrUABbb/eHLxStXko0k="
 
 $DevCenterEndpoint = Get-ServiceEndpoint -Context $distributedTaskContext -Name $serviceendpoint
 $tenantId = $DevCenterEndpoint.Authorization.Parameters.tenantid;
-#Write-Host "TenantId: $tenantId"
-
 $clientid = $DevCenterEndpoint.Authorization.Parameters.clientid;
-#Write-Host "ClientId: $clientid"
-
 $clientsecret = $DevCenterEndpoint.Authorization.Parameters.ApiToken;
+
+#Write-Host "TenantId: $tenantId"
+#Write-Host "ClientId: $clientid"
 #Write-Host "Client Secret: $clientsecret"
 
 #Write-Host "====AUTHORIZATION OBJECT===="
