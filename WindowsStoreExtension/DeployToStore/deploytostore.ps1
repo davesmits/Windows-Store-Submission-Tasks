172215a76b1249b0b04dfee6fbd0f6f7e89800c0
Dave Smits <dave@familie-smits.com>
2016-08-29 00:54:38 +0200

    powershell3 support for deploy to store task

--- WindowsStoreExtension/DeployToStore/deploytostore.ps1
+++ WindowsStoreExtension/DeployToStore/deploytostore.ps1
@@ -4,12 +4,12 @@ param
    [Parameter(Mandatory=$true)][string] $serviceendpoint,
    [Parameter(Mandatory=$true)][string] $appid,
    [Parameter(Mandatory=$false)][string] $flightid,
-   [Parameter(Mandatory=$true)][string] $file
+   [Parameter(Mandatory=$true)][string] $fileMask
 )
 
 
 
-$DevCenterEndpoint = Get-ServiceEndpoint -Context $distributedTaskContext -Name $serviceendpoint
+$DevCenterEndpoint = Get-VstsEndpoint -Name $serviceendpoint
 $tenantid = $DevCenterEndpoint.Authorization.Parameters.tenantid;
 $clientid = $DevCenterEndpoint.Authorization.Parameters.clientid;
 $clientsecret = $DevCenterEndpoint.Authorization.Parameters.ApiToken;
@@ -18,5 +18,18 @@ if (-Not $flightid){
     $flightid = "-";
 }
 
+$files = Find-VstsFiles $fileMask
+
+if ($files.length == 0)
+{
+    throw "n0 files found"
+    }
+
+if ($files.length > 1)
+{
+    throw "too many files found"
+}
+
+$file = $files[0]
 Write-Host "calling: .\StoreSubmission.exe ""$tenantid"" ""$clientid"" ""$clientsecret"" ""$appid"" ""$flightid"" ""$file"""
 .\StoreSubmission.exe "$tenantid" "$clientid" "$clientsecret" "$appid" "$flightid" "$file"
